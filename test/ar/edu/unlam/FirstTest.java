6d39bec1105750c83ed37ab2adb6b0a61cd40a82
MatiasLTonello <matiaslucianotonello11@gmail.com>
2023-09-20 21:00:11 -0300

    initialize project

---
+++ test/ar/edu/unlam/FirstTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
